4825f0f80e925723cc6817be9f70878098f5b062
Unknown <francescofavaro3@gmail.com>
2018-12-13 15:02:15 +0100

    _statistics
    la logica non è di casa in casa Zanon

--- battleship/src/app/services/data.service.ts
+++ battleship/src/app/services/data.service.ts
@@ -1,61 +1,53 @@
-import { Injectable } from '@angular/core';
-import { map } from 'rxjs/operators';
-import { HttpClient } from '@angular/common/http';
-//import { Observable } from 'rxjs/Observable';
-import { Router } from '@angular/router'
-
-@Injectable()
-export class DataService {
-  
-  _statistics = 'http://localhost:3000/user/';
-
-  constructor(public http:HttpClient, private _router: Router) { 
-  }
-
-  public deleteMessage(id : string){
-    this.http.delete('http://localhost:3000/messages/' + id).subscribe((data) =>
-    console.log('SUCC'));
-  }
-
-  public getUsers(){
-    return this.http.get('http://localhost:3000/user');
-          };
-
-  public getStatistics(username : string) {
-    return this.http.get(this._statistics + username + "/statistics")
-  };
-  
-  public getAdmin(){
-    return this.http.get('http://localhost:3000/user' )
-  }       
-
-  public upgradeUser(username : string){
-    return this.http.patch('http://localhost:3000/user/' + username + '/upgrade', null).subscribe((data) =>
-    console.log('SUCC'));
-  }
-
-  public downgradeUser(username : string){
-    return this.http.patch('http://localhost:3000/user/' + username + '/downgrade', null).subscribe((data) =>
-    console.log('SUCC'));
-  }
-
-  public userDelete(id: string): void{
-    this.http.delete('http://localhost:3000/user/' + id).subscribe((data) =>
-    console.log('SUCC'));
-  }
-
-  public getMessages(username : string){
-    return this.http.get('http://localhost:3000/messages/' + username);
-  }
-
-  /////////////////////////////////////////////////////////////////////////////////
-
-  public createConversation(conversationData){
-    return this.http.post<any>('http://localhost:3000/', conversationData);
-
-  }
-
-  
-} 
-
+import { Injectable } from '@angular/core'; 
+import { map } from 'rxjs/operators'; 
+import { HttpClient } from '@angular/common/http'; 
+//import { Observable } from 'rxjs/Observable'; 
+import { Router } from '@angular/router' 
+ 
+@Injectable() 
+export class DataService { 
+   
+  private _statistics = 'http://localhost:3000/user/'; 
+  private _messages = 'http://localhost:3000/messages/'; 
+  private _base = 'http://localhost:3000/'; 
+ 
+  constructor(public http:HttpClient, private _router: Router) {  
+  } 
+ 
+  public deleteMessage(id : string){ 
+    this.http.delete(this._messages + id).subscribe((data) => 
+    console.log('SUCC')); 
+  } 
+ 
+  public getUsers(){ 
+    return this.http.get(this._statistics); 
+          }; 
+ 
+  public getStatistics(username : string) { 
+    return this.http.get(this._statistics + username + "/statistics") 
+  }; 
+   
+  public getAdmin(){ 
+    return this.http.get(this._statistics ) 
+  }        
+ 
+  public userDelete(id: string): void{ 
+    this.http.delete(this._statistics + id).subscribe((data) => 
+    console.log('SUCC')); 
+  } 
+ 
+  public getMessages(username : string){ 
+    return this.http.get(this._messages + username); 
+  } 
+ 
+  ///////////////////////////////////////////////////////////////////////////////// 
+ 
+  public createConversation(conversationData){ 
+    return this.http.post<any>(this._base, conversationData); 
+ 
+  } 
+ 
+   
+}  
+ 
   
